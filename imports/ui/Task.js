364f94e993c494e59e3b9c9543f567441c2b6e4b
miyass <exe.miyakura0629@gmail.com>
2017-10-24 10:03:52 -0700

    Chapter5 is done.

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import { Tasks } from '../api/tasks.js';
+
 // Task component - represents a single todo item
 export default class Task extends Component {
+  toggleChecked() {
+    Tasks.update(this.props.task_id, {
+      $set: { checked: !this.props.task.checked },
+    });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task_id);
+  }
+
   render() {
+
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+        &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 }
